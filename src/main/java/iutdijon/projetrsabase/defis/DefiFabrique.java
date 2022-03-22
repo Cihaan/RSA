56c9a4b0f4f5be36ac687c3db24bdbbfb8f802f4
Cihaan <80070465+Cihaan@users.noreply.github.com>
2022-03-22 14:42:41 +0100

    modulo

--- src/main/java/iutdijon/projetrsabase/defis/DefiFabrique.java
+++ src/main/java/iutdijon/projetrsabase/defis/DefiFabrique.java
@@ -24,7 +24,9 @@ public class DefiFabrique {
             case 5 : defi = new Defi5EstInferieur(); break;
             case 6 : defi = new Defi6EstEgal(); break;
             case 7 : defi = new Defi7EstPair(); break;
+            case 8 : defi = new Defi8Multiplication(); break;
             case 9 : defi = new Defi9Quotient(); break;
+            case 10 : defi = new Defi10Modulo(); break;
             case 11 : defi = new Defi11RandomAvecTailleFixée(); break;
             default : throw new UnsupportedOperationException("Défis non implémenté !");
         }
